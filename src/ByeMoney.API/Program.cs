f34aa3546c27113421593ee0f15a89d39fe0fb5b
afsanehMovaghar <afsaneh.movaghar@gmail.com>
2026-08-23 16:15:04 +0330

    added serilog to project

--- src/ByeMoney.API/Program.cs
+++ src/ByeMoney.API/Program.cs
@@ -1,26 +1,55 @@
-﻿using ByeMoney.API;
+using ByeMoney.API;
 using ByeMoney.Application;
 using ByeMoney.Infrastructure;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(outputTemplate: "{UtcTimestamp:yyyy-MM-dd HH:mm:ss.fff} UTC [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .CreateBootstrapLogger();
 
-builder.Services.AddApplicationServices();
-builder.Services.AddInfrastructureServices(builder.Configuration);
-builder.Services.AddPresentationServices(builder.Configuration);
-
-var app = builder.Build();
-app.UseMiddleware<ByeMoney.API.Middleware.GlobalExceptionHandlingMiddleware>();
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting web application...");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console(outputTemplate: "{UtcTimestamp:yyyy-MM-dd HH:mm:ss.fff} UTC [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+        .WriteTo.File(
+            path: "logs/log-.txt",
+            rollingInterval: RollingInterval.Day,
+            retainedFileCountLimit: 14,
+            outputTemplate: "{UtcTimestamp:yyyy-MM-dd HH:mm:ss.fff} UTC [{Level:u3}] {Message:lj}{NewLine}{Exception}"));
+
+    builder.Services.AddApplicationServices();
+    builder.Services.AddInfrastructureServices(builder.Configuration);
+    builder.Services.AddPresentationServices(builder.Configuration);
+
+    var app = builder.Build();
+    app.UseMiddleware<ByeMoney.API.Middleware.GlobalExceptionHandlingMiddleware>();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
